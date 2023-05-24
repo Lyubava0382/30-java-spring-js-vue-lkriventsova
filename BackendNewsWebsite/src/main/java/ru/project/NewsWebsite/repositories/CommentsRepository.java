05a776cc1ca280c81a3d0605e448dd9708990c13
Lyubava0382 <lubovkrivencova@gmail.com>
2023-05-24 20:59:56 +0300

    Прототип новостной интернет-газеты (Java 8, Spring 5 - backend; js, vue - frontend)

---
+++ BackendNewsWebsite/src/main/java/ru/project/NewsWebsite/repositories/CommentsRepository.java
@@ -0,0 +1,12 @@
+package ru.project.NewsWebsite.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.project.NewsWebsite.models.Comment;
+
+import java.util.List;
+
+@Repository
+public interface CommentsRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findAllByOrderByCreatedAtDesc();
+}
